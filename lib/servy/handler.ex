bca6141e586f0f7889f10729d94ec0be2a5d68c9
Wasswa Samuel <latimerscope@gmail.com>
2019-10-12 20:22:32 +0200

    Another GenServer

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -32,17 +32,8 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
-    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
-
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1)end))
-      |> Enum.map(&Task.await/1)
-
-
-    where_is_bigfoot = Task.await(task)
-
-    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
+    sensor_data = Servy.SensorServer.get_sensor_data()
+    %{ conv | status: 200, resp_body: inspect sensor_data }
   end
 
 
