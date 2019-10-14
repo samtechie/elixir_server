caa6132cd2b5773ba0dd2c435b6d9904833b8936
Wasswa Samuel <latimerscope@gmail.com>
2019-10-14 22:10:37 +0200

    Fault Recovery with OTP supervisors part 1

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -9,9 +9,9 @@ defmodule Servy.PledgeServer do
     defstruct cache_size: 3, pledges: []
   end
 
-  def start do
+  def start_link(_arg) do
     IO.puts "Starting the pledge server .."
-    GenServer.start(__MODULE__,%State{}, name: @name)
+    GenServer.start_link(__MODULE__,%State{}, name: @name)
   end
   def create_pledge( name, amount) do
     GenServer.call @name, {:create_pledge, name, amount}
@@ -85,26 +85,26 @@ defmodule Servy.PledgeServer do
 end
 
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-{:ok, pid} = PledgeServer.start()
+# {:ok, pid} = PledgeServer.start()
 
-send pid, {:stop, "hammertime"}
+# send pid, {:stop, "hammertime"}
 
-PledgeServer.set_cache_size(4)
+# PledgeServer.set_cache_size(4)
 
-IO.inspect PledgeServer.create_pledge("larry", 10)
+# IO.inspect PledgeServer.create_pledge("larry", 10)
 
-# PledgeServer.clear()
+# # PledgeServer.clear()
 
-# IO.inspect PledgeServer.create_pledge("moe", 20)
-# IO.inspect PledgeServer.create_pledge("curly", 30)
-# IO.inspect PledgeServer.create_pledge("daisy", 40)
+# # IO.inspect PledgeServer.create_pledge("moe", 20)
+# # IO.inspect PledgeServer.create_pledge("curly", 30)
+# # IO.inspect PledgeServer.create_pledge("daisy", 40)
 
 
-IO.inspect PledgeServer.create_pledge("grace", 50)
+# IO.inspect PledgeServer.create_pledge("grace", 50)
 
-IO.inspect PledgeServer.recent_pledges()
-IO.inspect PledgeServer.total_pledged()
-IO.inspect Process.info(pid, :messages)
+# IO.inspect PledgeServer.recent_pledges()
+# IO.inspect PledgeServer.total_pledged()
+# IO.inspect Process.info(pid, :messages)
 
